080f434e171a701baf15e291e121a209c98f1a1c
antony@cosmologist.info <antony@cosmologist.info>
2012-09-21 13:40:35 +0100

    output best fit derived parameters

--- source/minimize.f90
+++ source/minimize.f90
@@ -52,7 +52,7 @@
     like = GetLogLike(P)
 
     call AcceptReject(.true., P%Info,MinParams%Info)
-    
+
     end function ffn
 
     function FindBestFit(Params,sigma_frac_err, max_iterations) result(best_like)
@@ -78,15 +78,71 @@
 
     npt = 2*num_params_used +1
     if (.not. BOBYQA (ffn, num_params_used ,npt, vect,XL,XU,rhobeg,rhoend,FeedBack+1, max_iterations)) &
-      stop 'FindBestFit failed'
+    stop 'FindBestFit failed'
 
     best_like = ffn(num_params_used,vect)
 
     !back to real units
     call VectToParams(vect, Params)
-    
+
     end function FindBestFit
 
+
+    subroutine WriteParamsHumanText(aunit, P, like)
+    use settings
+    use cmbtypes
+    use ParamDef
+    implicit none
+    Type(ParamSet) P
+    real, intent(in), optional :: like
+    integer, intent(in) :: aunit
+    Type(real_pointer) :: derived
+    integer numderived 
+    integer CalcDerivedParams
+    external CalcDerivedParams
+    integer isused,i
+
+    if (present(like)) then
+        write (aunit,*) '-log(Like) = ',like
+        write (aunit,*) ''
+    end if
+
+    do isused = 0,1
+        do i=1, num_real_params
+            if (isused==0 .and. Scales%PWidth(i)/=0 .or. isused==1 .and. Scales%PWidth(i)==0) then
+                write(aunit,'(1I5,1E15.7,"   '//trim(ParamNames_name(NameMapping,i))//'")') i, P%P(i)
+            end if 
+        end do
+        write (aunit,*) ''
+    end do
+
+    if (generic_mcmc) return
+
+    numderived = CalcDerivedParams(P, derived)
+    do i=1, numderived
+        write(aunit,'(1I5,1E15.7,"   '//trim(ParamNames_name(NameMapping,num_real_params + i ))//'")') &
+        num_real_params+i, derived%P(i)
+    end do
+    deallocate(derived%P)
+
+    if (nuisance_params_used>0) then
+        write (aunit,*) ''
+        do i=1, nuisance_params_used
+            write(aunit,'(1I5,1E15.7)') num_real_params+i, P%P(num_real_params+i)
+        end do
+    end if
+
+    end  subroutine WriteParamsHumanText
+
+#ifdef f2003
+use, intrinsic :: iso_fortran_env, only : input_unit=>stdin, &
+                                          output_unit=>stdout, &
+                                          error_unit=>stderr
+#else
+#define stdin  5
+#define stdout 6
+#define stderr 0
+#endif
 
     subroutine WriteBestFitParams(like, Params, fname)
     integer i
@@ -94,18 +150,13 @@
     Type(ParamSet) Params
     character(LEN=*), intent(in) :: fname
 
-    if (Feedback>0) write (*,*) 'Best fit parameters, -log(like) = ', like
+     
     call CreateTxtFile(fname,tmp_file_unit)
-    write (tmp_file_unit,*) 'Best fit sample -log(Like) = ',like
-    write (tmp_file_unit,*) ''
-    do i=1, num_params_used
-        write(tmp_file_unit,'(1I5,1E15.7,"   '//trim(ParamNames_name(NameMapping,params_used(i)))//'")') &
-           params_used(i), Params%P(params_used(i))
-        if (Feedback>0) write (*,'(1I5,1E15.7,"   '//trim(ParamNames_name(NameMapping,params_used(i)))//'")') &
-           params_used(i), Params%P(params_used(i))
-    end do
+    call WriteParamsHumanText(tmp_file_unit,Params, like)
     close(tmp_file_unit)
-
+    if (Feedback>0) call WriteParamsHumanText(stdout,Params, like)
+    
+   
     end subroutine WriteBestFitParams
 
     end module minimize
