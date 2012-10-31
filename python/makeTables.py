7569f5b17748bb0c5b65f9a812815001cb5da605
antony@cosmologist.info <antony@cosmologist.info>
2012-10-31 10:10:11 +0000

    python table fixes, running pdflatex

--- python/makeTables.py
+++ python/makeTables.py
@@ -9,23 +9,24 @@ if len(sys.argv) < 3:
     sys.exit()
 
 outfile = sys.argv[2]
+if outfile.find('.') < 0: outfile += '.tex'
 
 batchPath = os.path.abspath(sys.argv[1]) + os.sep
 batch = batchJob.readobject(batchPath + 'batch.pyobj')
 
 lines = []
 lines.append('\\documentclass[11pt]{article}')
-lines.append('\\begin{document}')
 lines.append('\\usepackage{fullpage}')
 lines.append('\\usepackage[landscape]{geometry}')
-lines.append('\\renewcommand{\arraystretch}{1.5}')
+lines.append('\\renewcommand{\\arraystretch}{1.5}')
+lines.append('\\begin{document}')
 
 
 for jobItem in batch.items():
     caption = jobItem.name.replace('_', '-')
     if len(sys.argv) > 3 and sys.argv[3] == '-bestfit':
         bf = ResultObjs.bestFit(jobItem.chainRoot + '.minimum')
-        caption = '$\\chi^2_{\\rm eff} = ' + ('%.2f' % bf.logLike) + '$'
+        caption += ' ($\\chi^2_{\\rm eff} = ' + ('%.2f' % bf.logLike) + '$)'
         lines.append(bf.resultTable(3, caption).tableTex())
     else:
         fname = jobItem.distPath + jobItem.name + '.margestats'
@@ -41,3 +42,5 @@ textFileHandle = open(outfile, 'w')
 textFileHandle.write("\n".join(lines))
 textFileHandle.close()
 
+print 'Now converting to PDF...'
+os.system('pdflatex ' + outfile)
