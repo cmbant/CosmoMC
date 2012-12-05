48a84c76cec420789bb846d4ce50dffee732dde8
antony@cosmologist.info <antony@cosmologist.info>
2012-12-05 20:40:27 +0000

    more options for runbatch, inc --notexist

--- python/runbatch.py
+++ python/runbatch.py
@@ -4,6 +4,9 @@ import os, batchJobArgs
 Opts = batchJobArgs.batchArgs('Submit jobs to run chains or importance sample')
 Opts.parser.add_argument('--nodes', type=int, default=2)
 Opts.parser.add_argument('--script', default='runMPI_HPCS.pl')
+Opts.parser.add_argument('--notexist', action='store_true')
+Opts.parser.add_argument('--dryrun', action='store_true')
+Opts.parser.add_argument('--subitems', action='store_true')
 
 (batch, args) = Opts.parseForBatch()
 
@@ -11,8 +14,9 @@ Opts.parser.add_argument('--script', default='runMPI_HPCS.pl')
 def submitJob(ini):
         command = 'perl ' + args.script + ' ' + ini + ' ' + str(args.nodes)
         print 'Submitting...' + command
-        os.system(command)
+        if not args.dryrun: os.system(command)
 
 
-for jobItem in Opts.filteredBatchItems(wantSubItems=False):
-        submitJob(jobItem.iniFile())
+for jobItem in Opts.filteredBatchItems(wantSubItems=args.subitems):
+        if not args.notexist or not os.path.exists(jobItem.chainRoot + '_1.txt'):
+            submitJob(jobItem.iniFile())
