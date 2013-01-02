0c58de1dca5349314c5296127b435e19ebbb39e2
antony@cosmologist.info <antony@cosmologist.info>
2013-01-02 23:43:22 +0000

    remove header from wmap sz template file

--- source/supernovae.f90
+++ source/supernovae.f90
@@ -11,14 +11,14 @@
     class(LikelihoodList) :: LikeList
     Type(TIniFile) :: ini
     integer count
-    
+
     if (.not. Ini_Read_Logical_File(Ini, 'use_SN',.false.)) return
 
     count = LikeList%Count
     call SNLSLikelihood_Add(LikeList, Ini)
     call Union2Likelihood_Add(LikeList, Ini)
     if (LikeList%Count > count+1) call MpiStop('SNLikelihood_Add: more than one - datasets not independent')
-    
+
     end subroutine SNLikelihood_Add
 
     end module snovae
