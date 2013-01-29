2404e46f843721bd5409cd1e5661e2ac2d74183b
antony@cosmologist.info <antony@cosmologist.info>
2013-01-29 16:52:15 +0000

    bestFitCAMB.py to make camb .ini

--- python/bestFitCAMB.py
+++ python/bestFitCAMB.py
@@ -1,4 +1,3 @@
-
 import ResultObjs, sys, os, iniFile
 
 
@@ -10,12 +9,10 @@ root = os.path.abspath(sys.argv[1])
 
 pars = {'ombh2':'omegabh2', 'omch2':'omegach2', 'omnuh2':'omeganuh2', 'hubble':'H0', 'w':'w',
         'helium_fraction':'yheused', 'scalar_amp(1)':'A' , 'scalar_spectral_index(1)':'ns', 'scalar_nrun(1)':'nrun', 'initial_ratio(1)':'r',
-        're_optical_depth':'tau', 're_delta_redshift':'deltazrei'}
+        're_optical_depth':'tau', 're_delta_redshift':'deltazrei', 'massless_neutrinos':'nnu'}
 
 ini = iniFile.iniFile()
-nmassive = 1
-ini.params['massless_neutrinos'] = 3.046 - nmassive
-ini.params['massive_neutrinos'] = nmassive
+
 ini.params['re_use_optical_depth'] = True
 ini.params['temp_cmb'] = 2.7255
 ini.params['CMB_outputscale'] = 2.7255e6 ** 2.
@@ -31,5 +28,13 @@ for camb, cosmomc in pars.items():
 
 ini.params['scalar_amp(1)'] = float(ini.params['scalar_amp(1)']) / 1e9
 
+nmassive = 1
+neffstandard = 3.046 / 3
+ini.params['massless_neutrinos'] = float(ini.params['massless_neutrinos']) - neffstandard * nmassive
+ini.params['massive_neutrinos'] = int(round(neffstandard * nmassive))
+ini.params['nu_mass_degeneracies'] = neffstandard * nmassive
+
 
 ini.saveFile(sys.argv[2])
+
+print 'warning: this does not support all parameter extensions from LCDM'
