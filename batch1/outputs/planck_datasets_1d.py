cb7536517c8c3be44d7b5a98bf034942b3578cab
antony@cosmologist.info <antony@cosmologist.info>
2013-02-18 09:04:11 +0000

    zrei plot

--- batch1/outputs/planck_datasets_1d.py
+++ batch1/outputs/planck_datasets_1d.py
@@ -11,3 +11,11 @@ g.export('planck_datasets_1d.eps')
 g.newPlot()
 g.plots_1d(roots,['omegabh2','omegach2','tau','A','ns','omegam','H0','sigma8'],nx=2, legend_labels=labels)
 g.export('outputs/planck_datasets_1d_params.eps')
+
+
+g.newPlot()
+labels=['Planck TT','Planck+Lensing','Planck+WP',]
+roots=['base_planck_CAMspec_lowl','base_planck_CAMspec_lowl_post_lensing','base_planck_CAMspec_lowl_lowLike']
+
+g.plots_1d(roots,['tau','zrei','A','sigma8'],nx=2, legend_labels=labels)
+g.export('outputs/reion_tau_1D.eps')
