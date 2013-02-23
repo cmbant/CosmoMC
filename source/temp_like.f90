5a1d254824655ac09dd0275ce76fd3824a141706
antony@cosmologist.info <antony@cosmologist.info>
2013-02-23 14:26:25 +0000

    tidies

--- source/temp_like.f90
+++ source/temp_like.f90
@@ -22,7 +22,8 @@
     real(campc) :: bestlike = 1e30_campc
     integer :: tempinstance
 
-    integer, allocatable :: indices(:),indices_reverse(:),keep_indices(:),keep_indices_reverse(:)
+    integer, allocatable :: marge_indices(:),marge_indices_reverse(:)
+    integer, allocatable :: keep_indices(:),keep_indices_reverse(:)
     real(campc), allocatable :: beam_conditional_mean(:,:)
     logical, allocatable :: want_marge(:)
     integer marge_num, keep_num
@@ -102,7 +103,7 @@
 
     CAMspec_lmax = maxval(lmaxX)
     allocate(fid_cl(CAMspec_lmax,4))
- !   allocate(fid_theory(CAMspec_lmax))
+    !   allocate(fid_theory(CAMspec_lmax))
 
     !open(48, file='./data/base_planck_CAMspec_lowl_lowLike_highL.bestfit_cl', form='formatted', status='unknown')
     !do i=2,CAMspec_lmax
@@ -140,8 +141,8 @@
     want_marge(1)=.false.
     marge_num=count(want_marge)
     keep_num=cov_dim-marge_num
-    allocate(indices(marge_num))
-    allocate(indices_reverse(cov_dim))
+    allocate(marge_indices(marge_num))
+    allocate(marge_indices_reverse(cov_dim))
     allocate(keep_indices(keep_num))
     allocate(keep_indices_reverse(cov_dim))
     print *,'beam marginalizing:',marge_num,'keeping',keep_num
@@ -151,41 +152,41 @@
     do i=1,cov_dim
         if (want_marge(i)) then
             j=j+1
-            indices(j) = i
+            marge_indices(j) = i
         else
             k=k+1
             keep_indices(k)=i
         end if
-        indices_reverse(i)=j
+        marge_indices_reverse(i)=j
         keep_indices_reverse(i)=k
     end do
     allocate(beam_cov(marge_num, marge_num))
-    beam_cov = beam_cov_inv(indices,indices)
+    beam_cov = beam_cov_inv(marge_indices,marge_indices)
     call Matrix_Inverse(beam_cov)
 
-        do if2=1,beam_Nspec
-            do if1=1,beam_Nspec
-                do ie2=1,num_modes_per_beam
-                    do ie1=1,num_modes_per_beam
-                        ii=ie1+num_modes_per_beam*(if1-1)
-                        jj=ie2+num_modes_per_beam*(if2-1)
-                        if (want_marge(ii) .and. want_marge(jj)) then
-                            do L2 = lminX(if2),lmaxX(if2)
-                                c_inv(npt(if1):npt(if1)+lmaxX(if1)-lminX(if1),npt(if2)+L2 -lminX(if2) ) = &
-                                c_inv(npt(if1):npt(if1)+lmaxX(if1)-lminX(if1),npt(if2)+L2 -lminX(if2) ) + &
-                                beam_modes(ie1,lminX(if1):lmaxX(if1),if1)* &
-                                beam_cov(indices_reverse(ii),indices_reverse(jj)) * beam_modes(ie2,L2,if2) &
-                                *fid_cl(L2,if2)*fid_cl(lminX(if1):lmaxX(if1),if1)
-                            end do
-                        end if
-                    enddo
+    do if2=1,beam_Nspec
+        do if1=1,beam_Nspec
+            do ie2=1,num_modes_per_beam
+                do ie1=1,num_modes_per_beam
+                    ii=ie1+num_modes_per_beam*(if1-1)
+                    jj=ie2+num_modes_per_beam*(if2-1)
+                    if (want_marge(ii) .and. want_marge(jj)) then
+                        do L2 = lminX(if2),lmaxX(if2)
+                            c_inv(npt(if1):npt(if1)+lmaxX(if1)-lminX(if1),npt(if2)+L2 -lminX(if2) ) = &
+                            c_inv(npt(if1):npt(if1)+lmaxX(if1)-lminX(if1),npt(if2)+L2 -lminX(if2) ) + &
+                            beam_modes(ie1,lminX(if1):lmaxX(if1),if1)* &
+                            beam_cov(marge_indices_reverse(ii),marge_indices_reverse(jj)) * beam_modes(ie2,L2,if2) &
+                            *fid_cl(L2,if2)*fid_cl(lminX(if1):lmaxX(if1),if1)
+                        end do
+                    end if
                 enddo
             enddo
         enddo
+    enddo
     ! print *,'after', c_inv(500,500), c_inv(npt(3)-500,npt(3)-502),  c_inv(npt(4)-1002,npt(4)-1000)
 
     allocate(beam_conditional_mean(marge_num, keep_num))
-    beam_conditional_mean=-matmul(beam_cov, beam_cov_inv(indices,keep_indices))
+    beam_conditional_mean=-matmul(beam_cov, beam_cov_inv(marge_indices,keep_indices))
     deallocate(beam_cov_inv)
     allocate(beam_cov_inv(keep_num,keep_num))
     beam_cov_inv = beam_cov_full(keep_indices,keep_indices)
@@ -262,10 +263,9 @@
     xi = freq_params(13)
     A_ksz = freq_params(14)
 
-
     beam_params = freq_params(num_non_beam+1:num_non_beam+cov_dim)
     !set marged beam parameters to their mean subject to fixed non-marged modes
-    beam_params(indices) = matmul(beam_conditional_mean, freq_params(num_non_beam+keep_indices))
+    beam_params(marge_indices) = matmul(beam_conditional_mean, beam_params(keep_indices))
 
     do ii=1,beam_Nspec
         do jj=1,num_modes_per_beam
@@ -353,69 +353,6 @@
 
     zlike=zlike+((cal2/cal1-0.9966d0)/0.0015d0)**2  +((cal0/cal1-1.0006d0)/0.0004d0)**2
 
-    ! print *,'CAMspec time:',  MPI_Wtime() - atime
-
-    ! WARNING; weakening prior...
-    !    zlike=zlike+((cal2/cal1-1.0056d0)/0.0063d0)**2 &
-    !               +((cal0/cal1-1.0127d0)/0.003d0)**2
-
-
-    ! correction to improve old way of handling cals...
-    !zlike=zlike &
-    !-2.d0*(lmaxX(1)-lminX(1)+1)*log(cal0) &
-    !-2.d0*(lmaxX(2)-lminX(2)+1)*log(cal1) &
-    !-2.d0*(lmaxX(3)-lminX(3)+1)*log(cal2) &
-    !-2.d0*(lmaxX(4)-lminX(4)+1)*.5d0*(log(cal1)+log(cal2))
-
-
-    !    if(writebest) then
-    !    if(zlike.lt.bestlike) then
-    !       open(49,file=bestname,form='formatted',status='unknown')
-    !       !do if1=1,beam_Nspec
-    !       !write(49,101) (beam_coeffs(if1,ie1),ie1=1,num_modes_per_beam)
-    !       !enddo
-    !101    format(5(e12.4))
-    !       do l = 0,2500
-    !          write(49,*) l, l*(l+1)*cell_cmb(l)
-    !       enddo
-    !       close(49)
-    !       bestlike=zlike
-    !    endif
-    !    endif
-
-    !if(storeall) then
-    !countnum=countnum+1
-    !write(storenumstring,*) countnum
-    !storename=trim(storeroot)//'_'//trim(adjustl(bestnum))//'_' &
-    !     //trim(adjustl(storenumstring))//'.txt'
-    !   open(49,file=storename,form='formatted',status='unknown')
-    !   do l=0,2500
-    !   write(49,*) l,l*(l+1)*cell_cmb(l)
-    !   enddo
-    !   write(49,*)
-    !   write(49,*) 'A_ps_100=',A_ps_100
-    !   write(49,*) 'A_ps_143=',A_ps_143
-    !   write(49,*) 'A_ps_217=',A_ps_217
-    !   write(49,*) 'A_cib_143=',A_cib_143
-    !   write(49,*) 'A_cib_217=',A_cib_217
-    !   write(49,*) 'A_sz=',A_sz
-    !   write(49,*) 'r_ps=',r_ps
-    !   write(49,*) 'r_cib=',r_cib
-    !   write(49,*) 'xi=',xi
-    !   write(49,*) 'A_ksz=',A_ksz
-    !   write(49,*) 'cal0=',cal0
-    !   write(49,*) 'cal1=',cal1
-    !   write(49,*) 'cal2=',cal2
-    !   do i=1,4
-    !   do j=1,num_modes_per_beam
-    !      write(49,*) 'beam_coeffs(',i,',',j,')=',beam_coeffs(i,j)
-    !      enddo
-    !      enddo
-    !      write(49,*)
-    !      write(49,*) 'zlike(=chi^2)=',zlike
-    !
-    !
-    !endif
     contains
 
     real(campc) function corrected_beam(spec_num,l)
