1ad5d134a03f99dc16f640abf76561ff529a3312
antony@cosmologist.info <antony@cosmologist.info>
2013-02-27 10:50:50 +0000

    added checkConverge --checkpoint

--- python/checkConverge.py
+++ python/checkConverge.py
@@ -3,26 +3,32 @@ import os, fnmatch, batchJobArgs
 Opts = batchJobArgs.batchArgs('Find chains which have failed or not converged.', importance=True, converge=True)
 
 Opts.parser.add_argument('--exist', action='store_true')
+Opts.parser.add_argument('--checkpoint', action='store_true')
 
 (batch, args) = Opts.parseForBatch()
 
 notExist = []
 converge = []
 
-for jobItem in Opts.filteredBatchItems():
-    if not jobItem.chainExists():
-        notExist.append(jobItem)
-    elif args.converge == 0 or not jobItem.hasConvergeBetterThan(args.converge, returnNotExist=True):
-        converge.append(jobItem)
+if args.checkpoint:
+    for jobItem in Opts.filteredBatchItems():
+        R, done = jobItem.convergeStat()
+        if R is not None and not done: print '...', jobItem.chainRoot, R
+else:
+    for jobItem in Opts.filteredBatchItems():
+        if not jobItem.chainExists():
+            notExist.append(jobItem)
+        elif args.converge == 0 or args.checkpoint or not jobItem.hasConvergeBetterThan(args.converge, returnNotExist=True):
+            converge.append(jobItem)
 
-print 'Checking batch (from last runGridGetdist.py output):'
-if not args.exist and len(notExist) > 0:
-    print 'Not exist...'
-    for jobItem in notExist:
-        print '...', jobItem.chainRoot
+    print 'Checking batch (from last runGridGetdist.py output):'
+    if not args.exist and len(notExist) > 0:
+        print 'Not exist...'
+        for jobItem in notExist:
+            print '...', jobItem.chainRoot
 
-print 'Converge check...'
-for jobItem in converge:
-    print '...', jobItem.chainRoot, jobItem.R()
+    print 'Converge check...'
+    for jobItem in converge:
+        print '...', jobItem.chainRoot, jobItem.R()
 
 
