e8994b4e40a096ea6c676b5199eac77b4f95190b
antony@cosmologist.info <antony@cosmologist.info>
2013-02-27 11:03:22 +0000

    added not_running

--- python/checkConverge.py
+++ python/checkConverge.py
@@ -4,6 +4,7 @@ Opts = batchJobArgs.batchArgs('Find chains which have failed or not converged.',
 
 Opts.parser.add_argument('--exist', action='store_true')
 Opts.parser.add_argument('--checkpoint', action='store_true')
+Opts.parser.add_argument('--not_running', action='store_true')
 
 (batch, args) = Opts.parseForBatch()
 
@@ -13,13 +14,14 @@ converge = []
 if args.checkpoint:
     for jobItem in Opts.filteredBatchItems():
         R, done = jobItem.convergeStat()
-        if R is not None and not done: print '...', jobItem.chainRoot, R
+        if R is not None and not done:
+            if not args.not_running or jobItem.notRunning(): print '...', jobItem.chainRoot, R
 else:
     for jobItem in Opts.filteredBatchItems():
         if not jobItem.chainExists():
             notExist.append(jobItem)
         elif args.converge == 0 or args.checkpoint or not jobItem.hasConvergeBetterThan(args.converge, returnNotExist=True):
-            converge.append(jobItem)
+            if not args.not_running or jobItem.notRunning(): converge.append(jobItem)
 
     print 'Checking batch (from last runGridGetdist.py output):'
     if not args.exist and len(notExist) > 0:
