c283cabec6f748d12ea357ebc97a2de4bb28aff2
antony@cosmologist.info <antony@cosmologist.info>
2013-03-02 11:56:01 +0000

    plik settings

--- python/settings_mini.py
+++ python/settings_mini.py
@@ -27,7 +27,7 @@ g.datasets = []
 # lists of dataset names to combine, with corresponding sets of inis to include
 # g.datasets.append([[planck, lowl, lowLike], ['CAMspec_nonclik.ini', 'lowl.ini', 'lowLike.ini']])
 # g.datasets.append([[planck, lowl, lowLike, highL], ['CAMspec_ACTSPT_nonclik.ini', 'lowl.ini', 'lowLike.ini']])
-g.datasets.append([[plik, lowl, lowLike], ['PLik_defaults.ini', 'lowl.ini', 'lowLike.ini']])
+g.datasets.append([[plik, lowl, lowLike], ['PLik_CAMspec_defaults.ini', 'lowl.ini', 'lowLike.ini']])
 
 class importanceFilterPlanck:
     def wantImportance(self, jobItem):
