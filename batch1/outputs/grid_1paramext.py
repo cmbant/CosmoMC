df84d098d1ee1e3142304605a7f28c100b701128
antony@cosmologist.info <antony@cosmologist.info>
2013-03-03 20:15:39 +0000

    plot

--- batch1/outputs/grid_1paramext.py
+++ batch1/outputs/grid_1paramext.py
@@ -12,7 +12,7 @@ g.settings.solid_colors = ['#009966', '#000866', '#000866']
 bases=['omegabh2','omegach2','ns','H0','sigma8']
 params=['omegak','mnu','nnu','yhe','nrun','r','w']
 defs=[0,0.06,3.046,0.247816,0,0,-1]
-bfs=[0.02209,0.1194,0.962,67.5,0.827]
+bfs=[0.02205,0.1199,0.9619,67.04,0.8347]
 
 
 roots=[['base_'+param+'_planck_lowl_lowLike','base_'+param+'_planck_lowl_lowLike_post_BAO','base_'+param+'_planck_lowl_lowLike_BAO'] for param in params]
