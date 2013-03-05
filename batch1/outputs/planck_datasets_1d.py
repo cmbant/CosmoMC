2e577cfb3913c61f471f3dd33cf65f08fc1aec53
antony@cosmologist.info <antony@cosmologist.info>
2013-03-05 08:26:57 +0000

    horizontal 2d

--- batch1/outputs/planck_datasets_1d.py
+++ batch1/outputs/planck_datasets_1d.py
@@ -7,8 +7,12 @@ roots=['base_planck_lowl','base_planck_lowl_post_lensing','base_planck_lowl_lowL
 g.plots_1d(roots, legend_labels=labels)
 g.exportExtra('planck_datasets_1d')
 
-g.newPlot()
-g.plots_1d(roots,['omegabh2','omegach2','thetastar','ns','omegam','H0','tau','zrei','A','sigma8'],nx=2, legend_labels=labels)
+#g.newPlot()
+#g.plots_1d(roots,['omegabh2','omegach2','thetastar','ns','omegam','H0','tau','zrei','A','sigma8'],nx=2, legend_labels=labels)
+#g.export('planck_datasets_1d_params')
+
+.newPlot()
+g.plots_1d(roots,['omegabh2','thetastar','A','tau','omegam','omegach2','ns','sigma8','zrei','H0'],nx=5, legend_ncol=4,legend_labels=labels)
 g.export('planck_datasets_1d_params')
 
 
