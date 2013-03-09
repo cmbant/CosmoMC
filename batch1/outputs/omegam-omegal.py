d7819a9ce442c1d5a467b8d3b8935492889a2d59
antony@cosmologist.info <antony@cosmologist.info>
2013-03-09 10:24:58 +0000

    zorder on filled contours

--- batch1/outputs/omegam-omegal.py
+++ batch1/outputs/omegam-omegal.py
@@ -18,8 +18,8 @@ g.newPlot()
 g.setAxes(params, lims=[0.2, 0.5, 0.5, 0.8])
 
 g.add_3d_scatter(roots[0], params)
-g.add_2d_contours(roots[2], params[0], params[1], filled=True, zorder=2)
 g.add_2d_contours(roots[1], params[0], params[1], filled=False, zorder=1)
+g.add_2d_contours(roots[2], params[0], params[1], filled=True, zorder=2, alpha=0.85)
 
 g.add_line([1, 0], [0, 1], zorder=0)
 
