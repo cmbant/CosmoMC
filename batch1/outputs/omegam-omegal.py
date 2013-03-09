53a82bf44257cfce08b91980f1dd657a338a9f0d
antony@cosmologist.info <antony@cosmologist.info>
2013-03-09 10:16:26 +0000

    omegak plot fix

--- batch1/outputs/omegam-omegal.py
+++ batch1/outputs/omegam-omegal.py
@@ -10,7 +10,7 @@ params = g.get_param_array(roots[0], ['omegam', 'omegal', 'H0'])
 g.setAxes(params, lims=[0, 1, 0, 1])
 g.add_3d_scatter(roots[0], params)
 g.add_2d_contours(roots[1], params[0], params[1], filled=False)
-g.add_2d_contours(roots[2], params[0], params[1], filled=True)
+#g.add_2d_contours(roots[2], params[0], params[1], filled=True)
 g.add_line([1, 0], [0, 1], zorder=1)
 g.export('Omegam-Omegal-H0')
 
@@ -18,8 +18,9 @@ g.newPlot()
 g.setAxes(params, lims=[0.2, 0.5, 0.5, 0.8])
 
 g.add_3d_scatter(roots[0], params)
-g.add_2d_contours(roots[1], params[0], params[1], filled=False)
 g.add_2d_contours(roots[2], params[0], params[1], filled=True)
+g.add_2d_contours(roots[1], params[0], params[1], filled=False, zorder=0)
+
 g.add_line([1, 0], [0, 1], zorder=0)
 
 g.add_legend(['+lensing','+lensing+BAO'])
