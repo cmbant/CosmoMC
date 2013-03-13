11d62785ecffd8db429051ba415ee99de2655f02
antony@cosmologist.info <antony@cosmologist.info>
2013-03-13 12:47:00 +0000

    updates to figures for neutrino section

--- batch1/outputs/nnu.py
+++ batch1/outputs/nnu.py
@@ -1,29 +1,23 @@
 import planckStyle as s
 from pylab import *
 
-g=s.plotter
-g.settings.line_labels = False
-g.settings.setWithSubplotSize(4)
+g=s.getSinglePlotter()
 
-g.settings.tight_layout=True
-g.settings.lab_fontsize=32
-g.settings.axes_fontsize = 24
-
-g.setAxes(lims=[2.0, 5.0, 0.0, 1.1])
+g.settings.lineM = ['-k', '-b', '-r', '-g', '-m', '-y']
 
 roots=['base_nnu_planck_lowl_lowLike_highL','base_nnu_planck_lowl_lowLike_highL_BAO', 'base_nnu_planck_lowl_lowLike_highL_post_HST', 'base_nnu_planck_lowl_lowLike_highL_BAO_post_HST']
 
-g.add_1d(roots[0],'nnu');
-g.add_1d(roots[1],'nnu',color='b');
-g.add_1d(roots[2],'nnu',color='r');
-g.add_1d(roots[3],'nnu',color='g');
+par = g.check_param(roots[0],'nnu')
 
-text(2.1,1.02, s.WPhighL, fontsize=24)
-text(2.1,0.92, '+BAO', color='b', fontsize=24)
-text(2.1,0.82, r'+$H_0$', color='r', fontsize=24)
-text(2.1,0.72, '+BAO+$H_0$', color='g', fontsize=24)
+#g.setAxes([par],lims=[2.0, 5.0, 0.0, 1.1])
 
-xlabel(r'$N_{\rm eff}$',fontsize=32)
-ylabel(r'$P/P_{\rm max}$',fontsize=32)
+g.plot_1d(roots,par);
+
+xlim([2.0,4.8])
+sz=9
+text(2.1,1.01, s.WPhighL, fontsize=9)
+text(2.1,0.92, '+BAO', color='b', fontsize=9)
+text(2.1,0.82, r'+$H_0$', color='r', fontsize=9)
+text(2.1,0.72, '+BAO+$H_0$', color='g', fontsize=9)
 
 g.export('nnu')
