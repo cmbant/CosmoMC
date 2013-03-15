fa2249ac3ac99777a5d27b297e71736427c390cb
antony@cosmologist.info <antony@cosmologist.info>
2013-03-15 18:10:34 +0000

    meffsterile plot; chains.py functions; more 1d plot options

--- batch1/outputs/msterile_nnu.py
+++ batch1/outputs/msterile_nnu.py
@@ -3,11 +3,18 @@ from pylab import *
 
 g=s.getSinglePlotter()
 
+
 g.plot_3d('base_nnu_meffsterile_planck_lowl_lowLike_highL', ['meffsterile', 'nnu', 'omegach2'], lims=[0, 3, 3.046, 4.9])
 
-m = arange(0.0, 3, 0.1)
-dN = (m / 6) ** 4.3
-plot(m, dN, '--k')
+
+m = np.arange(0, 3.0, 0.01)
+N = np.arange(3.047, 4.9, 0.01)
+N, m = np.meshgrid(N, m)
+z = m / (N - 3.046) ** 0.75
+CS = contour(m, N, z, origin='lower', levels=[0.5, 1, 2, 5, 10], colors='k', linestyles='--',linewidths=0.3, extent=[0,3,3.046,4.9])
+clabel(CS, CS.levels, inline=True,fmt='%1.1f',fontsize=7)
+xlim([0, 3])
+ylim([3.046, 4.9])
 
 g.export('msterile-nnu')
 
