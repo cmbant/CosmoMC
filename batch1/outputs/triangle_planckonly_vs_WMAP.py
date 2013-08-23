35c8106b059e2c79f3d578fdd8ac0dad578a307c
antony@cosmologist.info <antony@cosmologist.info>
2013-08-23 21:38:33 +0100

    support for default dashes style and dash customization

--- batch1/outputs/triangle_planckonly_vs_WMAP.py
+++ batch1/outputs/triangle_planckonly_vs_WMAP.py
@@ -7,7 +7,6 @@ g=s.getSubplotPlotter()
 
 roots = ['base_planck_lowl_post_lensing', 'base_planck_lowl_lowLike','base_WMAP']
 labs=[s.lensing, s.WP,'WMAP-9']
-#labs=['Planck+lensing','Planck+WP','WMAP-9']
 
 g.settings.plot_args= [None,None, {'color': 'gray', 'alpha':0.8}]
 g.triangle_plot(roots, ['omegabh2', 'omegach2', 'ns', 'tau', 'omegal', ], plot_3d_with_param='H0', filled_compare=False,
