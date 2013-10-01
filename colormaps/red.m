1dd74e5e87655a7015c7fbf8279b0e0ec222ca36
antony@cosmologist.info <antony@cosmologist.info>
2013-10-01 19:17:12 +0100

    deleted files for public

--- colormaps/red.m
+++
@@ -1,6 +0,0 @@
-%% red colormap
-cmap = colormap(gray);
-cmap(:,2)=cmap(length(cmap):-1:1,1);
-cmap(:,1)=1;
-cmap(:,3)=cmap(:,2);
-colormap(cmap);
--- colormaps/yellow.m
+++
@@ -1,6 +0,0 @@
-%% cyan colormap
-cmap = colormap(gray);
-cmap(:,3)=cmap(length(cmap):-1:1,1);
-cmap(:,1)=1;
-cmap(:,2)=1;
-colormap(cmap);
