b6a4a5fad7baf4d603a91e5dd2eecff77fded944
Antony Lewis <antony@cosmologist.info>
2014-04-09 09:42:11 +0100

    update python/makePerfectForecastDataset.py

--- python/makePerfectForecastDataset.py
+++ python/makePerfectForecastDataset.py
@@ -1,34 +1,41 @@
-# take CAMB file (e.g. test_lensedCls.dat') and produce dataset with given noise for testing
+# take CAMB file (e.g. test_lensedCls.dat) and produce dataset with given noise for testing
+# Use in cosmomc .ini file using e.g.
+# cmb_dataset[MyForecast]=data/MyForecast/test_lensedCls_exactsim.dataset
+
 from numpy import *
 import iniFile
 import shutil
 import sys, os
 
 
+# Edit parameters you want to change here
 
-# # Edit parameters you want to change here
+root = r'test_lensedCls'
 
-lensedTotClFileRoot = 'flatLCDM_lensedCls'
-reconNoise = 'reconNoise.dat'
-lensPotential = 'flatLCDM_lenspotentialCls.dat'
+lensedTotClFileRoot = r'c:\work\dist\git\camb\test_lensedCls'
+outDir = 'z:\\data\\'
+
 fwhm_arcmin = 7.
 # Noise var is N_l in muK^2 for white noise
 NoiseVar = 2e-4
-ENoiseFac = 2
+# Pol noise var = ENoiseFac * NoiseVar
+ENoiseFac = 4
 lmin = 2
 lmax = 2000
 fsky = 1
 
-DoLensing = True
+DoLensing = False
+# if including an idealised lensing reconstruction
+reconNoise = 'reconNoise.dat'
+lensPotential = 'flatLCDM_lenspotentialCls.dat'
 
 
-outDir = 'data/'
 # os.path.dirname(sys.path[0])+'/data/'
 ini = iniFile.iniFile()
 dataset = ini.params
 
 # change this if you don't want to use all pol
-dataset['fields_use'] = 'T E B'
+dataset['fields_use'] = 'T E'
 
 
 # #Now produce the Planck_like files
@@ -36,8 +43,8 @@ fwhm = fwhm_arcmin / 60
 xlc = 180 * sqrt(8.*log(2.)) / pi
 sigma2 = (fwhm / xlc) ** 2
 
-outPath = '%DATASETDIR%'
-outRoot = lensedTotClFileRoot + '_exactsim'
+outPath = ''
+outRoot = root + '_exactsim'
 NoiseOut = []
 for l in range(lmax + 1):
     NoiseCl = l * (l + 1) / 2 / pi * NoiseVar * exp(l * (l + 1) * sigma2)
@@ -51,25 +58,21 @@ outfile.close()
 
 dataset['fullsky_exact_fksy'] = fsky
 dataset['name'] = outRoot
-dataset['dataset_format'] = 'CMBLike'
-dataset['lowl_exact'] = False
-dataset['highl_cl'] = True
-dataset['like_approx'] = 3
+dataset['dataset_format'] = 'CMBLike2'
+dataset['like_approx'] = 'exact'
 
-
-dataset['bin_width'] = 1
 dataset['cl_lmin'] = lmin
 dataset['cl_lmax'] = lmax
 
+dataset['binned'] = False
+
+
 dataset['cl_hat_includes_noise'] = False
-dataset['cl_hat_includes_pointsources'] = False
+
 dataset['cl_hat_file'] = outPath + outRoot + '.dat'
 dataset['cl_hat_order'] = 'TT EE BB TE'
 dataset['cl_noise_file '] = outPath + outRoot + '_Noise.dat'
 dataset['cl_noise_order'] = 'TT EE BB'
-dataset['cl_noise_includes_pointsources'] = False
-dataset['point_source_cl'] = ''
-dataset['beam_modes_number'] = 0
 
 if DoLensing:
     dataset['lensing_recon_ncl'] = 1
