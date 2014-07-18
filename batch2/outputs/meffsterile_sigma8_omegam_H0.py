2f236b4ccaee64f7cef3c949d32b3a14a1755a1f
Antony Lewis <antony@cosmologist.info>
2014-07-18 15:26:55 +0100

    minor

--- batch2/outputs/meffsterile_sigma8_omegam_H0.py
+++ batch2/outputs/meffsterile_sigma8_omegam_H0.py
@@ -4,7 +4,8 @@ from pylab import *
 g=s.getSinglePlotter()
 
 g.make_figure(1, xstretch=1.3)
-roots=['base_nnu_meffsterile_'+s.defdata_TT]
+base='base_nnu_meffsterile_'
+roots=[base+s.defdata_TT]
 
 s8 =  np.arange(0.5, 1,0.01)
 
@@ -29,7 +30,9 @@ plotBounds(CFTHlens,'burlywood')
 plotBounds(PLSZ,'gray')    
 
 
-
 g.plot_3d(roots, ['sigma8', 'omegam','H0'])
+
+g.add_2d_contours(base+s.defdata_all+'_post_BAO', 'sigma8', 'omegam')
+#g.add_2d_contours(base+s.defdata_all+'_post_HST70p6', 'sigma8', 'omegam', plotno=1)
 
 g.export()
