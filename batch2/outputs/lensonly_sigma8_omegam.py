fix limit scaling in 2d GetDistPlots

batch2/outputs/lensonly_sigma8_omegam.py
@@ -6,12 +6,23 @@ g = s.getSinglePlotter()
 g.make_figure(1, xstretch=1.3)
 
 
+s8 = np.arange(0.5, 1, 0.01)
+
+def lensing(sigma):  # mandelbaum
+    return  ((0.572 + 0.019 * sigma) / s8) ** (1 / 0.25)
+
+def plotBounds(data, c):
+    fill_between(s8, data(-2), data(2), facecolor=c, alpha=0.15, edgecolor=c, lw=0)
+    fill_between(s8, data(-1), data(1), facecolor=c, alpha=0.25, edgecolor=c, lw=0)
+
+plotBounds(lensing, 'grey')
+
 g.plot_3d('base_lensonly', ['sigma8', 'omegam', 'H0'])
 
-g.add_2d_contours('base_lensonly_HST70p6', 'sigma8', 'omegam', ls='-', color='blue')
-g.add_2d_contours('base_lensonly_BAO', 'sigma8', 'omegam', ls='-', color='green')
-g.add_2d_contours('base_lensonly_theta', 'sigma8', 'omegam', ls='-', color='red')
-g.add_2d_contours('base_' + s.defdata_all, 'sigma8', 'omegam', ls='-', color='black')
+g.add_2d_contours('base_lensonly_HST70p6', 'sigma8', 'omegam', ls='-', color='blue', lw=0.6)
+g.add_2d_contours('base_lensonly_BAO', 'sigma8', 'omegam', ls='-', color='green', lw=0.6)
+g.add_2d_contours('base_lensonly_theta', 'sigma8', 'omegam', ls='-', color='red', lw=0.6)
+g.add_2d_contours('base_' + s.defdata_all, 'sigma8', 'omegam', ls='-', color='black', lw=1.2)
 
 # g.add_2d_contours('base_lensonly_HST70p6_widerns', 'sigma8', 'omegam', ls='--', color='orange')
 
@@ -22,9 +33,10 @@ if False:
     g.add_2d_contours('base_mnu_lensonly_theta', 'sigma8', 'omegam', ls='--', color='red')
 
 
+
 g.add_legend(['lensonly+HST', 'lensonly+BAO', 'lensonly+theta', 'Planck'])
 
 xlim([0.6, 1.02])
-ylim([0.13, 0.6])
+ylim([0.14, 0.6])
 
 g.export()

batch2/outputs/meffsterile_sigma8_omegam_H0.py
@@ -1,38 +1,38 @@
 import planckStyle as s
 from pylab import *
 
-g=s.getSinglePlotter()
+g = s.getSinglePlotter()
 
 g.make_figure(1, xstretch=1.3)
-base='base_nnu_meffsterile_'
-roots=[base+s.defdata_TT]
+base = 'base_nnu_meffsterile_'
+roots = [base + s.defdata_TT]
 
-s8 =  np.arange(0.5, 1,0.01)
+s8 = np.arange(0.5, 1, 0.01)
 
 
 def PLSZ(sigma):
-    #from Anna 18/7/2014
-    return  ((0.757+0.013*sigma)/s8)**(1/0.3)*0.32
+    # from Anna 18/7/2014
+    return  ((0.757 + 0.013 * sigma) / s8) ** (1 / 0.3) * 0.32
 
 def CFTHlens(sigma):
-    return  ((0.79+0.03*sigma)/s8)**(1/0.6)*0.27
+    return  ((0.79 + 0.03 * sigma) / s8) ** (1 / 0.6) * 0.27
 
-def galaxygalaxy(sigma): #mandelbaum
-    return  ((0.8+0.05*sigma)/s8)**(1/0.25)*0.25
+def galaxygalaxy(sigma):  # mandelbaum
+    return  ((0.8 + 0.05 * sigma) / s8) ** (1 / 0.57) * 0.25
 
 def plotBounds(data, c):
     fill_between(s8, data(-2), data(2), facecolor=c, alpha=0.15, edgecolor=c, lw=0)
     fill_between(s8, data(-1), data(1), facecolor=c, alpha=0.25, edgecolor=c, lw=0)
-    
-    
-#plotBounds(galaxygalaxy,'green')    
-plotBounds(CFTHlens,'burlywood')    
-plotBounds(PLSZ,'gray')    
 
 
-g.plot_3d(roots, ['sigma8', 'omegam','H0'])
+# plotBounds(galaxygalaxy,'green')
+plotBounds(CFTHlens, 'burlywood')
+plotBounds(PLSZ, 'gray')
 
-g.add_2d_contours(base+s.defdata_all+'_post_BAO', 'sigma8', 'omegam')
-#g.add_2d_contours(base+s.defdata_all+'_post_HST70p6', 'sigma8', 'omegam', plotno=1)
+
+g.plot_3d(roots, ['sigma8', 'omegam', 'H0'])
+
+g.add_2d_contours(base + s.defdata_all + '_post_BAO', 'sigma8', 'omegam')
+# g.add_2d_contours(base+s.defdata_all+'_post_HST70p6', 'sigma8', 'omegam', plotno=1)
 
 g.export()