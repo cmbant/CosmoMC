a7040d64abc9f899f851236c82ec762ca4d6e559
Antony Lewis <antony@cosmologist.info>
2014-10-15 13:37:47 +0100

    option for listGrid.py

--- python/listGrid.py
+++ python/listGrid.py
@@ -3,6 +3,7 @@ import batchJobArgs
 
 Opts = batchJobArgs.batchArgs('List items in a grid', importance=True, converge=True, notExist=True)
 Opts.parser.add_argument('--exists', action='store_true', help='chain must exist')
+Opts.parser.add_argument('--normed', action='store_true', help='Output normed names')
 
 
 (batch, args) = Opts.parseForBatch()
@@ -10,4 +11,7 @@ items = Opts.sortedParamtagDict(chainExist=args.exists)
 
 for paramtag, parambatch in items:
     for jobItem in parambatch:
-        print jobItem.name
+        if args.normed:
+            print jobItem.normed_name
+        else:
+            print jobItem.name
