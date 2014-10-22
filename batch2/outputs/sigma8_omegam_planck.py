1753904469a4f280428451561881e798b45e006f
Antony Lewis <antony@cosmologist.info>
2014-10-22 22:23:32 +0100

    plot tweaks

--- batch2/outputs/sigma8_omegam_planck.py
+++ batch2/outputs/sigma8_omegam_planck.py
@@ -5,7 +5,7 @@ g = s.getSinglePlotter()
 
 g.make_figure(1, xstretch=1.3)
 
-ranges = [0.245, 0.37, 0.73, 0.92]
+ranges = [0.245, 0.37, 0.73, 0.96]
 pair = ['omegam', 'sigma8']
 
 for TT in [False, True]:
@@ -31,13 +31,13 @@ for TT in [False, True]:
 
     if TT:
         g.add_2d_contours('base_' + s.defdata_TT, param_pair=pair, ls='--', color='magenta')
-        g.add_2d_contours('base_' + s.defdata_TTonly + '_post_WMAPtau', param_pair=pair, ls='-', color='brown', alpha=0.2)
+#        g.add_2d_contours('base_' + s.defdata_TTonly + '_post_WMAPtau', param_pair=pair, ls='-', color='brown', alpha=0.2)
     else:
         g.add_2d_contours('base_' + s.defdata_all, param_pair=pair, ls='--', color='magenta')
-        g.add_2d_contours('base_' + s.defdata_allNoLowE + '_post_WMAPtau', param_pair=pair, ls='-', color='brown', alpha=0.2)
+#        g.add_2d_contours('base_' + s.defdata_allNoLowE + '_post_WMAPtau', param_pair=pair, ls='-', color='brown', alpha=0.2)
 
     g.add_text(allname, 0.96, 0.12, color='magenta')
-    g.add_text(basedatname + '+$\\tau(0.09\pm 0.013)$', 0.96, 0.06, color='brown', alpha=0.4)
+#    g.add_text(basedatname + '+$\\tau(0.09\pm 0.013)$', 0.96, 0.06, color='brown', alpha=0.4)
 
     g.add_legend(legends, legend_loc='upper right', colored_text=True);
 
@@ -56,16 +56,16 @@ g.add_2d_contours('base_' + s.defdata_all, param_pair=pair, ls='-', color='olive
 g.add_2d_contours(g.getRoot('', s.defdata_all + '_lensing'), param_pair=pair, ls='-', color='midnightblue')
 # g.add_2d_contours(g.getRoot('', s.defdata_all + '_lensing_BAO'), 'sigma8', 'omegam', ls='-', color='pink')
 
-g.add_2d_contours('base_' + s.defdata_TTonly + '_post_WMAPtau', param_pair=pair, ls='--', color='brown', alpha=0.2)
+# g.add_2d_contours('base_' + s.defdata_TTonly + '_post_WMAPtau', param_pair=pair, ls='--', color='brown', alpha=0.2)
 
 # g.add_2d_contours('base_' + s.defdata_allNoLowE + '_lowtau', 'sigma8', 'omegam', color='red', filled=True)
 
 
-legends = [s.planckTT, s.NoLowLE, '+lensing', '+BAO']
+legends = [s.planckTT, s.NoLowLE, r'+lensing', r'+BAO']
 
 g.add_text(s.planckall, 0.96, 0.18, color='olive')
 g.add_text(s.planckall + '+lensing', 0.96, 0.12, color='midnightblue')
 
-g.add_text(s.planckTT + '+$\\tau(0.09\pm 0.013)$', 0.96, 0.06, color='brown', alpha=0.4)
+# g.add_text(s.planckTT + '+$\\tau(0.09\pm 0.013)$', 0.96, 0.06, color='brown', alpha=0.4)
 g.add_legend(legends, legend_loc='upper right', colored_text=True);
 g.export()
--- python/planckStyle.py
+++ python/planckStyle.py
@@ -23,7 +23,7 @@ rcParams.update(params)
 
 non_final = False
 version = 'clik10.1'
-defdata_root = 'plikHM'
+defdata_root = 'CamSpecHM'
 defdata_TT = defdata_root + '_TT_lowTEB'
 defdata_TE = defdata_root + '_TE_lowTEB'
 defdata_EE = defdata_root + '_EE_lowTEB'
@@ -37,10 +37,10 @@ defdata = defdata_TT
 planck = r'\textit{Planck}'
 planckTT = r'\textit{Planck} TT'
 planckTTlowTEB = r'\textit{Planck} TT$+$lowP'
-planckall = r'\textit{Planck} TT\,TE\,EE$+$lowP'
-NoLowLE = r'\textit{Planck} TT\,TE\,EE'
+planckall = r'\textit{Planck} TT,TE,EE$+$lowP'
+NoLowLE = r'\textit{Planck} TT,TE,EE'
 lensing = r'\textit{Planck} TT$+$lowP$+$lensing'
-lensingall = r'\textit{Planck} TT\,TE\,EE$+$lowP$+$lensing'
+lensingall = r'\textit{Planck} TT,TE,EE$+$lowP$+$lensing'
 
 defplanck = planckTTlowTEB
 
