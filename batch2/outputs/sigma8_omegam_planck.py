5d7e1ffd75280a4ae6ace4d8dffa645dcb093209
Antony Lewis <antony@cosmologist.info>
2014-10-28 10:37:21 +0000

    add zrei to computed plots

--- batch2/outputs/sigma8_omegam_planck.py
+++ batch2/outputs/sigma8_omegam_planck.py
@@ -8,40 +8,41 @@ g.make_figure(1, xstretch=1.3)
 ranges = [0.245, 0.37, 0.73, 0.96]
 pair = ['omegam', 'sigma8']
 
-for TT in [False, True]:
-    g.newPlot()
-    if not TT:
-        basedat = s.defdata_allNoLowE
-        basedatname = s.NoLowLE
-        allname = s.planckall
-        fname = 'Planckall'
-    else:
-        basedat = s.defdata_TTonly
-        basedatname = s.planckTT
-        fname = 'PlanckTT'
-        allname = s.planckTTlowTEB
+if False:
+    for TT in [False, True]:
+        g.newPlot()
+        if not TT:
+            basedat = s.defdata_allNoLowE
+            basedatname = s.NoLowLE
+            allname = s.planckall
+            fname = 'Planckall'
+        else:
+            basedat = s.defdata_TTonly
+            basedatname = s.planckTT
+            fname = 'PlanckTT'
+            allname = s.planckTTlowTEB
 
-    roots = [g.getRoot('', basedat),
-             g.getRoot('', basedat + '_lensing'),
-             g.getRoot('', basedat + '_lensing_BAO')]
+        roots = [g.getRoot('', basedat),
+                 g.getRoot('', basedat + '_lensing'),
+                 g.getRoot('', basedat + '_lensing_BAO')]
 
 
-    legends = [basedatname, '+lensing', '+BAO']
-    g.plot_2d(roots, param_pair=pair, filled=True, lims=ranges)
+        legends = [basedatname, '+lensing', '+BAO']
+        g.plot_2d(roots, param_pair=pair, filled=True, lims=ranges)
 
-    if TT:
-        g.add_2d_contours('base_' + s.defdata_TT, param_pair=pair, ls='--', color='magenta')
-#        g.add_2d_contours('base_' + s.defdata_TTonly + '_post_WMAPtau', param_pair=pair, ls='-', color='brown', alpha=0.2)
-    else:
-        g.add_2d_contours('base_' + s.defdata_all, param_pair=pair, ls='--', color='magenta')
-#        g.add_2d_contours('base_' + s.defdata_allNoLowE + '_post_WMAPtau', param_pair=pair, ls='-', color='brown', alpha=0.2)
+        if TT:
+            g.add_2d_contours('base_' + s.defdata_TT, param_pair=pair, ls='--', color='magenta')
+    #        g.add_2d_contours('base_' + s.defdata_TTonly + '_post_WMAPtau', param_pair=pair, ls='-', color='brown', alpha=0.2)
+        else:
+            g.add_2d_contours('base_' + s.defdata_all, param_pair=pair, ls='--', color='magenta')
+    #        g.add_2d_contours('base_' + s.defdata_allNoLowE + '_post_WMAPtau', param_pair=pair, ls='-', color='brown', alpha=0.2)
 
-    g.add_text(allname, 0.96, 0.12, color='magenta')
-#    g.add_text(basedatname + '+$\\tau(0.09\pm 0.013)$', 0.96, 0.06, color='brown', alpha=0.4)
+        g.add_text(allname, 0.96, 0.12, color='magenta')
+    #    g.add_text(basedatname + '+$\\tau(0.09\pm 0.013)$', 0.96, 0.06, color='brown', alpha=0.4)
 
-    g.add_legend(legends, legend_loc='upper right', colored_text=True);
+        g.add_legend(legends, legend_loc='upper right', colored_text=True);
 
-    g.export(fname + '_sigma_8-omega_m')
+        g.export(fname + '_sigma_8-omega_m')
 
 g.newPlot()
 dataroots = [s.defdata_TTonly, s.defdata_allNoLowE, s.defdata_allNoLowE + '_lensing', s.defdata_allNoLowE + '_lensing_BAO']
