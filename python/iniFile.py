36e1b4223cce2ef9a9d05c930122f6628fb0a469
Antony Lewis <antony@cosmologist.info>
2015-01-08 21:23:17 +0000

    error checking for ambiguous ini file includes

--- python/iniFile.py
+++ python/iniFile.py
@@ -15,47 +15,51 @@ class iniFile(object):
 
 
     def readFile(self, filename, keep_includes=False, if_not_defined=False):
-        fileincludes = []
-        filedefaults = []
-        self.original_filename = filename
-        textFileHandle = open(filename)
-        # Remove blanck lines and comment lines from the python list of lists.
-        for line in textFileHandle:
-            s = line.strip()
-            if s == 'END':break
-            if s.startswith('#'):continue
-            elif s.startswith('INCLUDE('):
-                fileincludes.append(s[s.find('(') + 1:s.rfind(')')])
-            elif s.startswith('DEFAULT('):
-                filedefaults.append(s[s.find('(') + 1:s.rfind(')')])
-            elif s != '':
-                eq = s.find('=')
-                if eq >= 0:
-                    key = s[0:eq].strip()
-                    if key in self.params:
-                        if if_not_defined: continue
-                        raise Exception('Error: duplicate key: ' + key)
-                    value = s[eq + 1:].strip()
-                    self.params[key] = value;
-                    self.readOrder.append(key);
+        try:
+            fileincludes = []
+            filedefaults = []
+            self.original_filename = filename
+            textFileHandle = open(filename)
+            # Remove blanck lines and comment lines from the python list of lists.
+            for line in textFileHandle:
+                s = line.strip()
+                if s == 'END':break
+                if s.startswith('#'):continue
+                elif s.startswith('INCLUDE('):
+                    fileincludes.append(s[s.find('(') + 1:s.rfind(')')])
+                elif s.startswith('DEFAULT('):
+                    filedefaults.append(s[s.find('(') + 1:s.rfind(')')])
+                elif s != '':
+                    eq = s.find('=')
+                    if eq >= 0:
+                        key = s[0:eq].strip()
+                        if key in self.params:
+                            if if_not_defined: continue
+                            raise Exception('Error: duplicate key: ' + key + ' in ' + filename)
+                        value = s[eq + 1:].strip()
+                        self.params[key] = value;
+                        self.readOrder.append(key);
 
-        textFileHandle.close()
-        if keep_includes:
-            self.includes += fileincludes
-            self.defaults += filedefaults
-        else:
-            for ffile in fileincludes:
-                if os.path.isabs(ffile):
-                    self.readFile(ffile)
-                else:
-                    self.readFile(os.path.join(os.path.dirname(filename), ffile))
-            for ffile in filedefaults:
-                if os.path.isabs(ffile):
-                    self.readFile(ffile, if_not_defined=True)
-                else:
-                    self.readFile(os.path.join(os.path.dirname(filename), ffile), if_not_defined=True)
+            textFileHandle.close()
+            if keep_includes:
+                self.includes += fileincludes
+                self.defaults += filedefaults
+            else:
+                for ffile in fileincludes:
+                    if os.path.isabs(ffile):
+                        self.readFile(ffile, if_not_defined=if_not_defined)
+                    else:
+                        self.readFile(os.path.join(os.path.dirname(filename), ffile), if_not_defined=if_not_defined)
+                for ffile in filedefaults:
+                    if os.path.isabs(ffile):
+                        self.readFile(ffile, if_not_defined=True)
+                    else:
+                        self.readFile(os.path.join(os.path.dirname(filename), ffile), if_not_defined=True)
 
-        return self.params
+            return self.params
+        except:
+            print 'Error in ' + filename
+            raise
 
     def saveFile(self, filename=None):
         if not filename: filename = self.original_filename
