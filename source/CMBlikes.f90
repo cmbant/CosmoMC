581273753d09651fdc6800c2b993e358df920f5e
Antony Lewis <antony@cosmologist.info>
2015-01-09 16:13:20 +0000

    edit for ifort 14 comptability

--- source/CMBlikes.f90
+++ source/CMBlikes.f90
@@ -510,6 +510,8 @@
     allocate(this%use_map(this%map_names%Count))
     S = Ini%Read_String('maps_use')
     if (S/='') then
+        if (any(.not. use_theory_field)) &
+            print *, 'CMBlikes WARNING: maps_use overrides fields_use'
         this%use_map = .false.
         call maps_use%SetFromString(S)
         do i=1,maps_use%Count
@@ -527,7 +529,8 @@
         end do
     end if
 
-    allocate(this%require_map(this%map_names%Count), source=this%use_map)
+    allocate(this%require_map(this%map_names%Count))
+    this%require_map = this%use_map
     !Bandpowers can depend on more fields than are actually used in likelihood
     !e.g. for correcting leakage or other linear corrections
     if (this%has_map_names) then
