2f8ee500badfed3f40c2052aa312af8a9a947fa7
Cora Dvorkin <cdvorkin@rclogin03.rc.fas.harvard.edu>
2015-01-21 15:37:36 -0500

    Added EE/BB ratio for both sync and dust.

--- source/CMB_BK_Planck.f90
+++ source/CMB_BK_Planck.f90
@@ -135,6 +135,7 @@
     real(mcp) :: fdust(this%nmaps_required)
     real(mcp) :: fsync(this%nmaps_required)
     real(mcp) :: dust, sync, dustsync
+    real(mcp) :: EEtoBB_dust,EEtoBB_sync    
     integer i,j,l
     real(mcp) :: lpivot = 80.0_mcp
 
@@ -146,6 +147,8 @@
     alphasync = DataParams(6)
     betasync = DataParams(7)
     dustsync_corr = DataParams(8)
+    EEtoBB_dust = DataParams(9)
+    EEtoBB_sync = DataParams(10)        
 
     do i=1, this%nmaps_required
         call DustScaling(betadust,Tdust,this%Bandpasses(i),fdust(i))
@@ -159,10 +162,21 @@
                 sync = fsync(i)*fsync(j)
                 dustsync = fdust(i)*fsync(j) + fsync(i)*fdust(j)
                 do l=this%pcl_lmin,this%pcl_lmax
-                    CL%CL(l) = CL%CL(l) + &
-                        dust*Adust*(l/lpivot)**(alphadust) + &
-                        sync*Async*(l/lpivot)**(alphasync) + &
-                        dustsync_corr*dustsync*sqrt(Adust*Async)*(l/lpivot)**((alphadust+alphasync)/2)
+                        if((i.eq.3).and.(j.eq.2))then
+                                CL%CL(l) = 0.d0
+                        end if
+                        if((i.eq.2).and.(j.eq.2))then 
+                                CL%CL(l) = CL%CL(l) + &
+                                dust*Adust*(l/lpivot)**(alphadust)*EEtoBB_dust + &
+                                sync*Async*(l/lpivot)**(alphasync)*EEtoBB_sync + &
+                                dustsync_corr*dustsync*sqrt(Adust*EEtoBB_dust*Async*EEtoBB_sync)*(l/lpivot)**((alphadust+alphasync)/2)
+                        end if
+                        if((i.eq.3).and.(j.eq.3))then
+                                CL%CL(l) = CL%CL(l) + &
+                                dust*Adust*(l/lpivot)**(alphadust) + &
+                                sync*Async*(l/lpivot)**(alphasync) + &
+                                dustsync_corr*dustsync*sqrt(Adust*Async)*(l/lpivot)**((alphadust+alphasync)/2)
+                        end if
                 end do
             end associate
         end do
