409bc80fb35f6a8274eec9c5cd9e73192948a733
Antony Lewis <antony@cosmologist.info>
2015-01-26 10:44:22 +0000

    update plots

--- batch2/outputs/ns_r_nrun.py
+++ batch2/outputs/ns_r_nrun.py
@@ -3,15 +3,25 @@ from pylab import *
 g = s.getSinglePlotter()
 
 g.settings.colorbar_rotation = -90
-roots = [s.defdata_TT, s.defdata_all]
-roots = ['base_nrun_r_' + root for root in roots] + ['base_r_' + s.defdata_all]
+dataroots = [s.defdata_TT, s.defdata_TT + '_lensing_BAO', s.defdata_TT + '_BKP_lensing_BAO']
+
+roots = [g.getRoot('nrun_r', s.defdata_TT),
+         g.getRoot('nrun_r', s.defdata_TT + '_lensing_BAO'),
+         g.getRoot('r', s.defdata_TT + '_lensing_BAO'),
+         g.getRoot('nrun_r', s.defdata_TT + '_BKP_lensing_BAO'),
+         g.getRoot('r', s.defdata_TT + '_BKP_lensing_BAO'),
+          ]
 
 nrun = g.param_latex_label(roots[0], 'nrun')
 labels = [s.LCDM + '+running+tensors', s.LCDM + '+tensors' ]
 
 # roots = ['base_nrun_r_' + s.defdata_all, 'base_r_' + s.defdata_all]
 
-g.plot_3d(roots, params_for_plots=[['ns02', 'r02', 'nrun'], ['ns02', 'r02'], ['ns', 'r02']], colors=['k', 'b'])
+g.plot_3d(roots, params_for_plots=[['ns02', 'r02', 'nrun'], ['ns02', 'r02'], ['ns', 'r02'], ['ns02', 'r02'], ['ns', 'r02']],
+          colors=['k', 'b', 'k', 'b'], ls=['-', '-', '--', '--'])
 
 g.add_legend(labels, legend_loc='upper left', colored_text=True)
+
+gca().set_xticks([0.9, 0.95, 1, 1.05, 1.1])
+
 g.export()
