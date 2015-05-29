a8b7bcca39066c9fa3c765dc997603e6c0c57389
Antony Lewis <antony@cosmologist.info>
2015-05-29 12:52:15 +0100

    update sample_settings.py

--- python/settings_sample.py
+++ python/settings_sample.py
@@ -34,10 +34,36 @@ g.importanceRuns.append([['BAO'], ['BAO.ini']])
 
 groups.append(g)
 
-# try to match each new run name to exisitng covmat
-# e.g. get name without particular data combinations
+# ranges for parameters when they are varied (can delete params if you just want to use defaults)
+params = dict()
+params['w'] = '-0.99 -3. 1 0.02 0.02'
+params['wa'] = '0 -3 2 0.05 0.05'
+params['mnu'] = '0.02 0 5 0.1 0.03'
+params['omegak'] = '-0.0008 -0.3 0.3 0.001 0.001'  # starting exactly on flat seems to confuse minimizer
+params['nnu'] = '3.046 0.05 10 0.05 0.05'
+params['nrun'] = '0 -1 1 0.005 0.001'
+params['r'] = '0 0 3 0.03 0.03'
+params['Alens'] = '1 0 10 0.05 0.05'
+params['yhe'] = '0.245 0.1 0.5 0.006 0.006'
+params['alpha1'] = '0 -1 1 0.0003 0.0003'
+params['meffsterile'] = '0.1 0 3 0.1 0.03'
+
+
+# extra parameters that are set only when specific parameters are varied. Can deleted to get defaults.
+param_extra_opts = {
+    'mnu': {'num_massive_neutrinos': 3},
+    'meffsterile': {'param[mnu]': '0.06', 'param[nnu]': '3.1 3.046 10 0.05 0.05', 'num_massive_neutrinos': 1,
+                    'accuracy_level': 1.2},
+    'yhe': {'bbn_consistency': False},
+    'r': {'compute_tensors': True},
+    'nt': {'inflation_consistency': False, 'lmax_tensor': 1000}
+}
+
+
+# try to match each new run name to exisitng covmat (covariance matrix for efficient exploration)
+# e.g. try to map to get name without particular data combinations
 covWithoutNameOrder = ['lensing', 'BAO']
-# or try replacing various names (these are standard for provided planck_covmats)
+# or try replacing various names (these are standard for the provided planck_covmats)
 covNameMappings = {'plikHM': 'plik', 'plikLite': 'plik'}
 # for mapping to nominal mission names try
 # covNameMappings = {'plikHM':'planck','TT':'', 'lowTEB':'lowLike'}
