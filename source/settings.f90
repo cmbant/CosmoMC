2d22124178541af693f09b38d5a27802de500525
Antony Lewis <antony@cosmologist.info>
2018-07-20 09:24:14 +0200

    updates for new astro parameterization etc; update version tag and docs

--- source/settings.f90
+++ source/settings.f90
@@ -34,7 +34,7 @@
 
     logical :: use_fast_slow = .false.
 
-    character(LEN=*), parameter :: CosmoMC_Version = 'Sept2017'
+    character(LEN=*), parameter :: CosmoMC_Version = 'July2018'
 
     character(LEN=:), allocatable :: chisq_label
 
