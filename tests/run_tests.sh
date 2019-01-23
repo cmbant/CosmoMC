d9de0047ef96b87292658c17e93948185f0e56ae
Antony Lewis <antony@cosmologist.info>
2019-01-23 22:17:02 +0000

    turn off planck test for devel docker

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,5 +1,9 @@
 
 cd /cosmomc
+
+if [[ $GCCTAG != "devel"]]
+then
+
 wget http://irsa.ipac.caltech.edu/data/Planck/release_2/software/COM_Likelihood_Code-v2.0.R2.00.tar.bz2
 tar xvfj COM_Likelihood_Code-v2.*.tar.bz2
 cd plc-2.0
@@ -12,11 +16,19 @@ tar xvfz COM_Likelihood_Data-baseline_R2.00.tar.gz
 ln -s $(pwd)/plc_2.0 ./data/clik
 rm -f COM_Likelihood_Data-baseline_R2.00.tar.gz
 
+fi
+
 make
 
 mpirun -np 1 --allow-run-as-root ./cosmomc test.ini
 
+if [[ $GCCTAG != "devel"]]
+then
+
 mpirun -np 1 --allow-run-as-root ./cosmomc test_planck.ini
+
+fi
+
 rc=$?
 
 
