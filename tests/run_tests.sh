4ec4e39a3a6ca5f8f6a989453028fb35c9ad025c
Antony Lewis <antony@cosmologist.info>
2019-02-25 15:47:45 +0000

    try new clik build

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,12 +1,10 @@
 
 cd /cosmomc
 
-if ["$GCCTAG" != "devel"]
-then
 
-wget http://irsa.ipac.caltech.edu/data/Planck/release_2/software/COM_Likelihood_Code-v2.0.R2.00.tar.bz2
-tar xvfj COM_Likelihood_Code-v2.*.tar.bz2
-cd plc-2.0
+wget https://cdn.cosmologist.info/cosmobox/plc-2.1_py3.tar.bz2
+tar xvfj *.tar.bz2
+cd plc*
 ./waf configure --install_all_deps
 ./waf install
 source ./bin/clik_profile.sh
@@ -16,20 +14,15 @@ tar xvfz COM_Likelihood_Data-baseline_R2.00.tar.gz
 ln -s $(pwd)/plc_2.0 ./data/clik
 rm -f COM_Likelihood_Data-baseline_R2.00.tar.gz
 
-fi
 
 make
 
 mpirun -np 1 --allow-run-as-root ./cosmomc test.ini
 rc=$?
 
-if ["$GCCTAG" != "devel"]
-then
+mpirun -np 1 --allow-run-as-root ./cosmomc test_planck.ini
+rc=$?
 
- mpirun -np 1 --allow-run-as-root ./cosmomc test_planck.ini
- rc=$?
-
-fi
 
 exit $rc
 
