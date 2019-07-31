69b200289797307849b93127e0fbce7c2e93fa1c
Antony Lewis <antony@cosmologist.info>
2019-07-31 10:53:28 +0200

    2018 test

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,12 +4,14 @@ make ReleaseMPI
 cd /cosmomc
 #wget http://irsa.ipac.caltech.edu/data/Planck/release_2/software/COM_Likelihood_Code-v2.0.R2.00.tar.bz2
 wget https://pla.esac.esa.int/pla/aio/product-action?COSMOLOGY.FILE_ID=COM_Likelihood_Code-v3.0_R3.00.tar.gz
+tar xvfj *.tar.gz
+cd code/plc_3.0
 tar xvfj *.tar.bz2
 cd plc-3.0
 ./waf configure --install_all_deps
 ./waf install
 source ./bin/clik_profile.sh
-cd ..
+cd ../../..
 #wget http://irsa.ipac.caltech.edu/data/Planck/release_2/software/COM_Likelihood_Data-baseline_R2.00.tar.gz
 wget https://pla.esac.esa.int/pla/aio/product-action?COSMOLOGY.FILE_ID=COM_Likelihood_Data-baseline_R3.00.tar.gz
 #tar xvfz COM_Likelihood_Data-baseline_R2.00.tar.gz
