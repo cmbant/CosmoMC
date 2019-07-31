838ced4955242726a086bd570de7720ff9d15ea6
Antony Lewis <antony@cosmologist.info>
2019-07-31 12:33:18 +0200

    update test links for 2018 release

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,20 +1,20 @@
 
 cd /cosmomc
-#wget http://irsa.ipac.caltech.edu/data/Planck/release_2/software/COM_Likelihood_Code-v2.0.R2.00.tar.bz2
-wget https://cdn.cosmologist.info/cosmobox/plc-3.0.tar.bz2
+wget https://pla.esac.esa.int/pla/aio/product-action?COSMOLOGY.FILE_ID=COM_Likelihood_Code-v3.0_R3.00.tar.gz
+tar xvf *.tar.gz
+cd code/plc_3.0
 tar xvfj *.tar.bz2
 cd plc-3.0
 ./waf configure --install_all_deps
 ./waf install
 source ./bin/clik_profile.sh
-cd ..
-#wget http://irsa.ipac.caltech.edu/data/Planck/release_2/software/COM_Likelihood_Data-baseline_R2.00.tar.gz
+cd ../../..
 wget https://cdn.cosmologist.info/cosmobox/test2019_kaml/baseline.tar.gz
-#tar xvfz COM_Likelihood_Data-baseline_R2.00.tar.gz
-tar xvfz baseline.tar.gz
+wget https://pla.esac.esa.int/pla/aio/product-action?COSMOLOGY.FILE_ID=COM_Likelihood_Data-baseline_R3.00.tar.gz
+tar xvfz *baseline*.tar.gz
 ln -s $(pwd)/baseline/plc_3.0 ./data/clik_14.0
 #rm -f COM_Likelihood_Data-baseline_R2.00.tar.gz
-rm -f baseline.tar.gz
+rm -f *baseline*.tar.gz
 
 
 make
