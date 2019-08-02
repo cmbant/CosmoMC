2b423921c28c27b0e8f662072ded03b8c55fee57
Antony Lewis <antony@cosmologist.info>
2019-08-02 19:46:37 +0200

    update PLC link

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,10 +1,8 @@
 
 cd /cosmomc
-wget https://pla.esac.esa.int/pla/aio/product-action?COSMOLOGY.FILE_ID=COM_Likelihood_Code-v3.0_R3.00.tar.gz
+wget http://pla.esac.esa.int/pla/aio/product-action?COSMOLOGY.FILE_ID=COM_Likelihood_Code-v3.0_R3.01.tar.gz
 tar xvf *.tar.gz
-cd code/plc_3.0
-tar xvfj *.tar.bz2
-cd plc-3.0
+cd code/plc_3.0/plc-3.01
 ./waf configure --install_all_deps
 ./waf install
 source ./bin/clik_profile.sh
@@ -12,7 +10,6 @@ cd ../../..
 wget https://pla.esac.esa.int/pla/aio/product-action?COSMOLOGY.FILE_ID=COM_Likelihood_Data-baseline_R3.00.tar.gz
 tar xvfz *baseline*.tar.gz
 ln -s $(pwd)/baseline/plc_3.0 ./data/clik_14.0
-#rm -f COM_Likelihood_Data-baseline_R2.00.tar.gz
 rm -f *baseline*.tar.gz
 
 
