c4cd840538bb07eb2570ecfa1e93997858affc1a
Antony Lewis <antony@cosmologist.info>
2022-01-13 21:12:24 +0000

    test

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,17 +1,6 @@
-gfortran -v
+git gfortran -v
 
 cd /cosmomc
-wget https://pla.esac.esa.int/pla/aio/product-action?COSMOLOGY.FILE_ID=COM_Likelihood_Code-v3.0_R3.01.tar.gz
-tar xvf *.tar.gz
-cd code/plc_3.0/plc-3.01
-./waf configure --install_all_deps
-./waf install
-source ./bin/clik_profile.sh
-cd ../../..
-wget https://pla.esac.esa.int/pla/aio/product-action?COSMOLOGY.FILE_ID=COM_Likelihood_Data-baseline_R3.00.tar.gz
-tar xvfz *baseline*.tar.gz
-ln -s $(pwd)/baseline/plc_3.0 ./data/clik_14.0
-rm -f *baseline*.tar.gz
 
 
 make
@@ -19,8 +8,6 @@ make
 mpirun -np 1 --allow-run-as-root ./cosmomc test.ini
 rc=$?
 
-mpirun -np 1 --allow-run-as-root ./cosmomc test_planck.ini
-rc=$?
 
 
 exit $rc
